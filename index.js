56ccc2a673021912c4c32d696e6a222cc05b5ad1
wangwentao97 <31856530+wangwentao97@users.noreply.github.com>
2022-10-26 19:40:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get('/userInfo', (req, res) => {
     
 }) 
 
-let port = process.env.PROT || 3000;
+let port = process.env.PORT || 3000;
 app.listen(3000, ()=> {
     console.log('listening at localhost:3000');
 })
